387ed8a69c4e3fc20e51fd020623da8c1a6f8f03
John Lee <jy.johnlee@gmail.com>
2014-07-01 23:07:38 -0700

    again

---
+++ Pod/Classes/polyPlot_initialize.c
@@ -0,0 +1,31 @@
+/*
+ * polyPlot_initialize.c
+ *
+ * Code generation for function 'polyPlot_initialize'
+ *
+ * C source code generated on: Tue Jul  1 16:42:21 2014
+ *
+ */
+
+/* Include files */
+#include "rt_nonfinite.h"
+#include "polyPlot.h"
+#include "polyPlot_initialize.h"
+
+/* Type Definitions */
+
+/* Named Constants */
+
+/* Variable Declarations */
+
+/* Variable Definitions */
+
+/* Function Declarations */
+
+/* Function Definitions */
+void polyPlot_initialize(void)
+{
+  rt_InitInfAndNaN(8U);
+}
+
+/* End of code generation (polyPlot_initialize.c) */
